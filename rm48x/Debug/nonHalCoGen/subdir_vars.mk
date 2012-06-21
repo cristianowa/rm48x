1b4f08ee40cdad9acef06f1df450403c82bbb35f
Cristiano Araujo <cristianowerneraraujo@gmail.com>
2012-06-21 18:07:35 -0300

    Standard code.

---
+++ rm48x/Debug/nonHalCoGen/subdir_vars.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../nonHalCoGen/interrupt_handlers.c \
+../nonHalCoGen/leds_control.c \
+../nonHalCoGen/user_main.c 
+
+OBJS += \
+./nonHalCoGen/interrupt_handlers.obj \
+./nonHalCoGen/leds_control.obj \
+./nonHalCoGen/user_main.obj 
+
+C_DEPS += \
+./nonHalCoGen/interrupt_handlers.pp \
+./nonHalCoGen/leds_control.pp \
+./nonHalCoGen/user_main.pp 
+
+OBJS__QTD += \
+".\nonHalCoGen\interrupt_handlers.obj" \
+".\nonHalCoGen\leds_control.obj" \
+".\nonHalCoGen\user_main.obj" 
+
+C_DEPS__QTD += \
+".\nonHalCoGen\interrupt_handlers.pp" \
+".\nonHalCoGen\leds_control.pp" \
+".\nonHalCoGen\user_main.pp" 
+
+C_SRCS_QUOTED += \
+"../nonHalCoGen/interrupt_handlers.c" \
+"../nonHalCoGen/leds_control.c" \
+"../nonHalCoGen/user_main.c" 
+
+
---
+++ rm48x/Debug/subdir_vars.mk
@@ -0,0 +1,173 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../dcan.c \
+../dcc.c \
+../dmm.c \
+../gio.c \
+../i2c.c \
+../lin.c \
+../mibadc.c \
+../mibspi.c \
+../nhet.c \
+../pinmux.c \
+../rti.c \
+../rtp.c \
+../sci.c \
+../spi.c \
+../sys_esm.c \
+../sys_main.c \
+../sys_phantom.c \
+../sys_startup.c \
+../system.c 
+
+ASM_SRCS += \
+../sys_core.asm \
+../sys_intvecs.asm \
+../sys_memory.asm \
+../sys_mpu.asm \
+../sys_pmu.asm 
+
+CMD_SRCS += \
+../sys_link.cmd 
+
+ASM_DEPS += \
+./sys_core.pp \
+./sys_intvecs.pp \
+./sys_memory.pp \
+./sys_mpu.pp \
+./sys_pmu.pp 
+
+OBJS += \
+./dcan.obj \
+./dcc.obj \
+./dmm.obj \
+./gio.obj \
+./i2c.obj \
+./lin.obj \
+./mibadc.obj \
+./mibspi.obj \
+./nhet.obj \
+./pinmux.obj \
+./rti.obj \
+./rtp.obj \
+./sci.obj \
+./spi.obj \
+./sys_core.obj \
+./sys_esm.obj \
+./sys_intvecs.obj \
+./sys_main.obj \
+./sys_memory.obj \
+./sys_mpu.obj \
+./sys_phantom.obj \
+./sys_pmu.obj \
+./sys_startup.obj \
+./system.obj 
+
+C_DEPS += \
+./dcan.pp \
+./dcc.pp \
+./dmm.pp \
+./gio.pp \
+./i2c.pp \
+./lin.pp \
+./mibadc.pp \
+./mibspi.pp \
+./nhet.pp \
+./pinmux.pp \
+./rti.pp \
+./rtp.pp \
+./sci.pp \
+./spi.pp \
+./sys_esm.pp \
+./sys_main.pp \
+./sys_phantom.pp \
+./sys_startup.pp \
+./system.pp 
+
+OBJS__QTD += \
+".\dcan.obj" \
+".\dcc.obj" \
+".\dmm.obj" \
+".\gio.obj" \
+".\i2c.obj" \
+".\lin.obj" \
+".\mibadc.obj" \
+".\mibspi.obj" \
+".\nhet.obj" \
+".\pinmux.obj" \
+".\rti.obj" \
+".\rtp.obj" \
+".\sci.obj" \
+".\spi.obj" \
+".\sys_core.obj" \
+".\sys_esm.obj" \
+".\sys_intvecs.obj" \
+".\sys_main.obj" \
+".\sys_memory.obj" \
+".\sys_mpu.obj" \
+".\sys_phantom.obj" \
+".\sys_pmu.obj" \
+".\sys_startup.obj" \
+".\system.obj" 
+
+ASM_DEPS__QTD += \
+".\sys_core.pp" \
+".\sys_intvecs.pp" \
+".\sys_memory.pp" \
+".\sys_mpu.pp" \
+".\sys_pmu.pp" 
+
+C_DEPS__QTD += \
+".\dcan.pp" \
+".\dcc.pp" \
+".\dmm.pp" \
+".\gio.pp" \
+".\i2c.pp" \
+".\lin.pp" \
+".\mibadc.pp" \
+".\mibspi.pp" \
+".\nhet.pp" \
+".\pinmux.pp" \
+".\rti.pp" \
+".\rtp.pp" \
+".\sci.pp" \
+".\spi.pp" \
+".\sys_esm.pp" \
+".\sys_main.pp" \
+".\sys_phantom.pp" \
+".\sys_startup.pp" \
+".\system.pp" 
+
+C_SRCS_QUOTED += \
+"../dcan.c" \
+"../dcc.c" \
+"../dmm.c" \
+"../gio.c" \
+"../i2c.c" \
+"../lin.c" \
+"../mibadc.c" \
+"../mibspi.c" \
+"../nhet.c" \
+"../pinmux.c" \
+"../rti.c" \
+"../rtp.c" \
+"../sci.c" \
+"../spi.c" \
+"../sys_esm.c" \
+"../sys_main.c" \
+"../sys_phantom.c" \
+"../sys_startup.c" \
+"../system.c" 
+
+ASM_SRCS_QUOTED += \
+"../sys_core.asm" \
+"../sys_intvecs.asm" \
+"../sys_memory.asm" \
+"../sys_mpu.asm" \
+"../sys_pmu.asm" 
+
+
